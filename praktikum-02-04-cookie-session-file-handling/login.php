bcc44857d3fb8ea42f34f8f697c4d26dc28da2b3
agusalim192 <alimagus192@gmail.com>
2023-01-31 13:39:29 +0800

    pratikum-02-04

---
+++ praktikum-02-04-cookie-session-file-handling/login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h2><center>Form Login</center></h2>
+    <form action="" method="post">
+   
+        <div class="container">
+             
+            <div class="row mb-3">
+                <label for="username" class="col-sm-5 col-form-label">Username</label>
+                <div class="col-sm-6">
+                <input type="text" class="form-control" name="username" placeholder="username">
+            </div>
+            </div>
+            <div class="row mb-3">
+                <label for="password" class="col-sm-5 col-form-label">Password</label>
+                <div class="col-sm-6">
+                <input type="password" class="form-control" name="password"  placeholder="password">
+                </div>
+            </div>
+
+            <?php
+            session_start();
+            $username = 'agus alim';
+            $password = '123';
+            if (isset($_POST['masuk'])) {
+                if ($_POST['username'] == $username && $_POST['password'] == $password){
+                    //Membuat Session
+                    $_SESSION["username"] = $username; 
+                    header("location: dashboard.php");
+                    echo "Nama Pangguna: ".$_SESSION[" username"];
+
+                } else {
+                    // Tampilkan Pesan Error
+                   
+                    echo '<p>Username Atau Password Tidak Benar</p>';
+                }
+            }  
+            ?>
+                <button type="submit" name="masuk" class="btn btn-success" value="login">Login</button>
+       
+            </div>
+        
+           
+    </form>
+</body>
+</html>
